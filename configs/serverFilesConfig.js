196e7f2f23694b298f17c7de4f97000993649228
unknown <thunderrk2001@gmail.com>
2021-09-17 18:00:35 +0530

    adding path in config file for memesScrapped2.json file

--- configs/serverFilesConfig.js
+++ configs/serverFilesConfig.js
@@ -1,5 +1,5 @@
 const obj = {
-    "json_file_scrap": "./serverFiles/memesScraped.json",
+    "json_file_scrap": ["./serverFiles/memesScraped.json", "./serverFiles/memesScrapped2.json"],
     "json_file_api": "./serverFiles/memesApi.json",
     "response_json_key": "memes"
 }
