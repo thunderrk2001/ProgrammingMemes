7bba56a1b2f489d58a1838773509af2394ce32f2
unknown <thunderrk2001@gmail.com>
2021-09-17 17:55:20 +0530

    endPoints urls added for new added url

--- configs/urls.js
+++ configs/urls.js
@@ -1,7 +1,7 @@
 const apiUrls = ["https://www.reddit.com/r/ProgrammerHumor/top.json?limit=30",
     "https://www.reddit.com/r/ProgrammerHumor/new.json?limit=30"
 ]
-const scrapUrls = ["https://programmerhumor.io/database-memes/",
+const scrapUrls1 = ["https://programmerhumor.io/database-memes/",
     "https://programmerhumor.io/reaction/funny-memes/",
     "https://programmerhumor.io/reaction/awesome-memes/",
     "https://programmerhumor.io/javascript-memes/",
@@ -9,4 +9,10 @@ const scrapUrls = ["https://programmerhumor.io/database-memes/",
     "https://programmerhumor.io/debugging-memes/",
     "https://programmerhumor.io/python-memes/",
 ]
-module.exports = { apiUrls, scrapUrls }
+const scrapUrls2 = ["https://devhumor.com/category/quote",
+    "https://devhumor.com",
+    "https://devhumor.com/popular",
+    "https://devhumor.com/category/code",
+    "https://devhumor.com/category/bugs"
+]
+module.exports = { "apiUrls": apiUrls, "scarpUrls": [scrapUrls1, scrapUrls2] }
